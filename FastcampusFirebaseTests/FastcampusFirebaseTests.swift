5be17e8f17eb6815c50779a945620cc005b95180
word <word@socar.kr>
2017-07-15 01:15:03 +0900

    - Initial Project

---
+++ FastcampusFirebaseTests/FastcampusFirebaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  FastcampusFirebaseTests.swift
+//  FastcampusFirebaseTests
+//
+//  Created by Janginsu on 2017. 7. 10..
+//  Copyright © 2017년 Janginsu. All rights reserved.
+//
+
+import XCTest
+@testable import FastcampusFirebase
+
+class FastcampusFirebaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-FastcampusFirebaseTests/Pods-FastcampusFirebaseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FastcampusFirebaseTests : NSObject
+@end
+@implementation PodsDummy_Pods_FastcampusFirebaseTests
+@end
